302947849d88f7c975917cb553758b9963296d0d
Anatol Derbisz <anatholius@gmail.com>
2021-04-06 17:03:21 +0200

    [`Client`] Refactoring to begin using some useful abstraction

--- src/Rest/Client.php
+++ src/Rest/Client.php
@@ -4,19 +4,33 @@ namespace App\Rest;
 
 use App\Rest\Transport\Curl;
 
+/**
+ * @property Curl transport
+ */
 class Client
 {
+    public function __construct(string $transportName = 'curl')
+    {
+        // try..catch is for avoiding annoying underlines in PhpStorm
+        try {
+            // `PHP 8` semantics for catch in `7.4`
+            $this->transport = match ($transportName) {
+                'curl' => new Curl(),
+                default => throw new \Exception('Unknown transport type')
+            };
+        } catch(\Exception $exception) {
+            var_dump($exception);
+            die();
+        }
+    }
+
     public function get(string $url): array
     {
-        $curl = new Curl();
-
-        return $curl->get($url);
+        return $this->transport->get($url);
     }
 
     public function post(string $url, array $params): array
     {
-        $curl = new Curl();
-
-        return $curl->post($url, $params);
+        return $this->transport->post($url, $params);
     }
 }
