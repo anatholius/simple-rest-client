8c29f19d7960e2d5984487ee1dd21ef0f30955e5
Anatol Derbisz <anatholius@gmail.com>
2021-04-06 22:42:49 +0200

    [Tests] Added test for api controller with succeed and failed responses (`beSuccess` inline matcher 😉) - but something wrong, let's find out

--- spec/Controller/ApiControllerSpec.php
+++ spec/Controller/ApiControllerSpec.php
@@ -4,10 +4,22 @@ namespace spec\App\Controller;
 
 use App\Controller\ApiController;
 use App\Rest\Response;
+use App\Rest\ResponseInterface;
+use JetBrains\PhpStorm\ArrayShape;
 use PhpSpec\ObjectBehavior;
 
 class ApiControllerSpec extends ObjectBehavior
 {
+    #[ArrayShape(['beSuccess' => "\Closure"])]
+    public function getMatchers(): array
+    {
+        return [
+            'beSuccess' => function(ResponseInterface $response) {
+                return $response->getResult()['success'];
+            },
+        ];
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(ApiController::class);
@@ -21,5 +33,39 @@ class ApiControllerSpec extends ObjectBehavior
     function it_should_allow_to_CreateOne()
     {
         $this->createOne()->shouldReturnAnInstanceOf(Response::class);
+    }
+
+    function it_should_allow_to_CreateOne_with_success()
+    {
+        $postData = [
+            'producer' => [
+                "id" => rand(500, 999),
+                "name" => "some name",
+                "site_url" => null,
+                "logo_filename" => "svg.file",
+                "ordering" => "123",
+                "source_id" => null,
+            ],
+        ];
+        $response = $this->createOne($postData);
+        $response->shouldReturnAnInstanceOf(ResponseInterface::class);
+        $response->shouldBeSuccess();
+    }
+
+    function it_should_allow_to_CreateOne_with_error()
+    {
+        $postData = [
+            'producer' => [
+                "id" => 500,
+                "name" => "some name",
+                "site_url" => null,
+                "logo_filename" => "svg.file",
+                "ordering" => "123",
+                "source_id" => null,
+            ],
+        ];
+        $response = $this->createOne($postData);
+        $response->shouldReturnAnInstanceOf(ResponseInterface::class);
+        $response->shouldNotBeSuccess();
     }
 }
