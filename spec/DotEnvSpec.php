798a56c44c4bddc038a2672cdbb978caaf7c93a3
Anatol Derbisz <anatholius@gmail.com>
2021-04-06 15:28:27 +0200

    [`DotEnv`] Added empty `.env` file

--- spec/DotEnvSpec.php
+++ spec/DotEnvSpec.php
@@ -3,6 +3,7 @@
 namespace spec\App;
 
 use App\DotEnv;
+use PhpSpec\Exception\Example\ErrorException;
 use PhpSpec\ObjectBehavior;
 
 class DotEnvSpec extends ObjectBehavior
@@ -12,6 +13,7 @@ class DotEnvSpec extends ObjectBehavior
         $this->shouldHaveType(DotEnv::class);
         $this->shouldNotThrow(\RuntimeException::class)->duringGetVars(true);
         $this->shouldThrow(\ArgumentCountError::class)->duringGetVar();
+        $this->shouldNotThrow(ErrorException::class)->duringGetVar('UNKNOWN_VAR');
         $this->getVars(true)->shouldBeArray();
     }
 }
