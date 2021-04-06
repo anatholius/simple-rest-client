2e5f89450a28a343a95231c648af70802b4d09bc
Anatol Derbisz <anatholius@gmail.com>
2021-04-06 16:06:37 +0200

    [`Transport/Curl`] Expected implementation of interface, created `TransportInterface` and implement it for `Curl` class

--- spec/Rest/Transport/CurlSpec.php
+++ spec/Rest/Transport/CurlSpec.php
@@ -3,6 +3,7 @@
 namespace spec\App\Rest\Transport;
 
 use App\Rest\Transport\Curl;
+use App\Rest\Transport\TransportInterface;
 use PhpSpec\ObjectBehavior;
 
 class CurlSpec extends ObjectBehavior
@@ -10,5 +11,6 @@ class CurlSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(Curl::class);
+        $this->shouldBeAnInstanceOf(TransportInterface::class);
     }
 }
--- src/Rest/Transport/TransportInterface.php
+++ src/Rest/Transport/TransportInterface.php
@@ -1,7 +1,9 @@
 <?php
 
+
 namespace App\Rest\Transport;
 
-class Curl implements TransportInterface
+
+interface TransportInterface
 {
 }
