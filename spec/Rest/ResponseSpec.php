5f38f35f2f66b4a1b98b80745f77634a536c4d33
Anatol Derbisz <anatholius@gmail.com>
2021-04-06 19:45:22 +0200

    [`Response`] Defined class abstraction and checked defaults, depending on the server structure

--- spec/Rest/ResponseSpec.php
+++ spec/Rest/ResponseSpec.php
@@ -11,4 +11,16 @@ class ResponseSpec extends ObjectBehavior
     {
         $this->shouldHaveType(Response::class);
     }
+
+    function it_should_allow_to_get_default_result_with_default_values()
+    {
+        $result = $this->getResult();
+        $result->shouldBeArray();
+        $result->shouldHaveKey('success');
+        $result['success']->shouldBeBool();
+        $result->shouldHaveKey('data');
+        $result['data']->shouldBeNull();
+        $result->shouldHaveKey('error');
+        $result['error']->shouldBeNull();
+    }
 }
--- src/Rest/Response.php
+++ src/Rest/Response.php
@@ -2,6 +2,21 @@
 
 namespace App\Rest;
 
+use JetBrains\PhpStorm\ArrayShape;
+
 class Response
 {
+    private bool $success = false;
+    private ?array $data = null;
+    private ?array $error = null;
+
+    #[ArrayShape(['success' => "mixed", 'data' => "mixed", 'error' => "mixed"])]
+    public function getResult(): array
+    {
+        return [
+            'success' => $this->success,
+            'data' => $this->data,
+            'error' => $this->error,
+        ];
+    }
 }
