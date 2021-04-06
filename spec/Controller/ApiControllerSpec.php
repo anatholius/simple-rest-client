601fe393b0f629a73f663ab546567af74d53271f
Anatol Derbisz <anatholius@gmail.com>
2021-04-06 21:22:44 +0200

    [`ApiController`] Designed `getAll()` function with using `Client` to get `Response`

--- spec/Controller/ApiControllerSpec.php
+++ spec/Controller/ApiControllerSpec.php
@@ -3,6 +3,7 @@
 namespace spec\App\Controller;
 
 use App\Controller\ApiController;
+use App\Rest\Response;
 use PhpSpec\ObjectBehavior;
 
 class ApiControllerSpec extends ObjectBehavior
@@ -10,5 +11,10 @@ class ApiControllerSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(ApiController::class);
+    }
+
+    function it_should_GetAll()
+    {
+        $this->getAll()->shouldReturnAnInstanceOf(Response::class);
     }
 }
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,20 @@
 
 namespace App\Controller;
 
+use App\Rest\Client;
+use App\Rest\Response;
+
 class ApiController
 {
+    public function getAll(): Response
+    {
+        $client = new Client();
+        $client->setTransport('curl');
+
+        $response = new Response();
+
+        // TODO: write logic here
+
+        return $response;
+    }
 }
