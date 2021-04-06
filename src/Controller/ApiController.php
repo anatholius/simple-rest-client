a1535d27a1452061febf6e7fc1a2e5710f3822bb
Anatol Derbisz <anatholius@gmail.com>
2021-04-06 22:22:12 +0200

    [Refactoring] Unifying the call context - `part.7` - unifying `restResponse` and using it in controllers; added `ext-http` to composer `require` section (this should be installed and configured on machine with php installation - can cause problems)

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,37 +6,51 @@ use App\Rest\Client;
 use App\Rest\Response;
 use App\Rest\ResponseInterface;
 use App\SimpleDotEnv;
+use HttpRequestException;
 
 class ApiController
 {
+    /**
+     * @return ResponseInterface
+     * @throws HttpRequestException
+     */
     public function getAll(): ResponseInterface
     {
         $routePath = SimpleDotEnv::getVar('GET_ALL_URL');
 
-        return $this->restResponse($routePath);
+        return $this->restResponse($routePath, 'GET');
     }
 
+    /**
+     * @param array|null $postData
+     *
+     * @return ResponseInterface
+     * @throws HttpRequestException
+     */
     public function createOne(?array $postData = []): ResponseInterface
     {
-        $client = new Client();
-
         $routePath = SimpleDotEnv::getVar('CREATE_ONE_URL');
-        $result = $client->post($routePath, $postData);
 
-        if($result instanceof ResponseInterface) {
-            $response = $result;
-        } else {
-            $response = new Response($result);
-        }
-
-        return $response;
+        return $this->restResponse($routePath, 'POST', $postData);
     }
 
-    protected function restResponse($routePath): ResponseInterface
+    /**
+     * @param string     $routePath
+     * @param string     $method
+     * @param array|null $postData
+     *
+     * @return ResponseInterface
+     * @throws HttpRequestException
+     */
+    protected function restResponse(string $routePath, string $method, ?array $postData = []): ResponseInterface
     {
         $client = new Client();
 
-        $result = $client->get($routePath);
+        $result = match ($method) {
+            'GET' => $client->get($routePath),
+            'POST' => $client->post($routePath, $postData),
+            default => throw new HttpRequestException(sprintf('Nie obsługiwana metoda: %s', $method))
+        };
 
         if($result instanceof ResponseInterface) {
             $response = $result;
